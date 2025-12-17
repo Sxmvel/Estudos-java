2095574d953a98a78c1a171658e956c8978ef931
Sxmvel <samuelres444@gmail.com>
2025-12-17 13:43:51 -0300

    Exercicio com while para encontrar o número secreto.

---
+++ src/fase1_Logica/Desafio08_Random.java
@@ -0,0 +1,37 @@
+package fase1_Logica;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Desafio08_Random {
+    public static void main(String[] args) {
+        Scanner t = new Scanner(System.in);
+
+        System.out.println("ADIVINHE UM NUMERO DE 0 A 100");
+
+        Random random = new Random();
+
+        int numero = random.nextInt(101);
+        System.out.println(numero);
+
+
+        int chute = -1;
+
+        while(chute != numero){
+            System.out.println("CHUTE UM NÚMERO");
+            chute = t.nextInt();
+
+            if(chute > numero){
+                System.out.println("CHUTE EH MAIOR");
+            }else{
+                String s = (chute < numero) ? "CHUTE EH MENOR" : " PARABENS VC ACERTOU O NUMERO";
+                System.out.println(s);
+            }
+        }
+
+
+
+
+
+    }
+}

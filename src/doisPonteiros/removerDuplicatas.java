3dfda11cff36ff8f1d7a72e99421786f4eedd86d
Sxmvel <samuelres444@gmail.com>
2026-03-17 10:21:09 -0300

    Dado um array de inteiros nums ordenado em ordem crescente, remova as duplicatas in-place (no próprio array) de forma que cada elemento exclusivo apareça apenas uma vez.

---
+++ src/doisPonteiros/removerDuplicatas.java
@@ -0,0 +1,22 @@
+package doisPonteiros;
+
+import java.util.Arrays;
+
+public class removerDuplicatas {
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {0,0,1,1,1,2,2,3,3,4,5,5,6,7,7,8,8,9};
+
+        int escritor = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i] != nums[i-1]){
+                nums[escritor] = nums[i]; // So chega aqui se o numero atual for diferente do anterior
+                escritor++;
+            }
+        }
+
+        System.out.println(escritor);
+
+    }
+}

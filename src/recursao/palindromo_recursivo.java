41893c1782ced2f6fc11ae57e822370a20c9e231
Samuel Resende <samuelres444@gmail.com>
2026-04-02 20:05:56 -0300

    Resumo recursividade

---
+++ src/recursao/palindromo_recursivo.java
@@ -0,0 +1,23 @@
+package recursao;
+
+public class palindromo_recursivo {
+
+    public static boolean isPalindromo(String palavra, int esquerda, int direita){
+
+        if(esquerda >= direita){
+            return true;
+        }else{
+            if(esquerda != direita{
+                return  false;
+            }
+        }else{
+            direita--;
+            esquerda++;
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+    }
+}
